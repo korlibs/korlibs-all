dfe3334ce52f7b33ff4de66685c6499cf60deb40
soywiz <soywiz@gmail.com>
2018-06-17 23:26:28 +0200

    Fixed test

--- korui/korui/common/test/com/soywiz/korui/BasicTest.kt
+++ korui/korui/common/test/com/soywiz/korui/BasicTest.kt
@@ -27,10 +27,10 @@ class BasicTest {
 				create(FRAME)=0
 				setProperty(0,LightProperty[TEXT],Title)
 				setBounds(0,0,0,640,480)
+				setBounds(0,0,0,640,480)
 				create(BUTTON)=1
 				setProperty(1,LightProperty[TEXT],Hello)
 				setParent(1,0)
-				setBounds(0,0,0,640,480)
 				setProperty(0,LightProperty[VISIBLE],true)
 				setBounds(1,0,0,640,480)
 				setBounds(0,0,0,640,480)
