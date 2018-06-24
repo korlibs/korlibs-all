b3bcaf49012de201360f064a333cbff3dc2b28ee
soywiz <soywiz@gmail.com>
2018-06-24 21:55:59 +0200

    WIP sample

---
+++ samples/sample1/src/example/Example1.kt
@@ -0,0 +1,21 @@
+package example
+
+import com.soywiz.korge.*
+import com.soywiz.korge.scene.*
+import com.soywiz.korge.view.*
+import com.soywiz.korinject.*
+
+fun main(args: Array<String>): Unit = Korge(MyModule)
+
+object MyModule : Module() {
+	override val mainScene = MyScene::class
+	override suspend fun init(injector: AsyncInjector) {
+		injector
+			.mapPrototype { MyScene() }
+	}
+}
+
+class MyScene : Scene() {
+	override suspend fun sceneInit(sceneView: Container) {
+	}
+}
