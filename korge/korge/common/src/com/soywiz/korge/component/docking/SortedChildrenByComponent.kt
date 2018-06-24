0e189b976a87ff37f7668ca3beed660c43721f8d
soywiz <soywiz@gmail.com>
2018-06-24 21:24:49 +0200

    kotlin-native Korge support

--- korge/korge/common/src/com/soywiz/korge/component/docking/SortedChildrenByComponent.kt
+++ korge/korge/common/src/com/soywiz/korge/component/docking/SortedChildrenByComponent.kt
@@ -10,10 +10,24 @@ class SortedChildrenByComponent(val container: Container, var comparator: Compar
 	}
 }
 
-fun <T : Container> T.keepChildrenSortedBy(comparator: Comparator<View>) =
+// @TODO: kotlin-native: kotlin.Comparator { }
+//             korge/korge/common/src/com/soywiz/korge/component/docking/SortedChildrenByComponent.kt:25:127: error: unresolved reference: Comparator
+// @TODO: kotlin-native: recursive problem!
+//korge/korge/common/src/com/soywiz/korge/component/docking/SortedChildrenByComponent.kt:18:140: error: cannot infer a type for this parameter. Please specify it explicitly.
+//      fun <T : Container, T2 : Comparable<T2>> T.keepChildrenSortedBy(selector: (View) -> T2): T = this.keepChildrenSortedBy(kotlin.Comparator { a, b -> selector(a).compareTo(selector(b)) })
+
+//fun <T : Container> T.keepChildrenSortedBy(comparator: Comparator<View>) = this.apply { SortedChildrenByComponent(this, comparator).attach() }
+//fun <T : Container, T2 : Comparable<T2>> T.keepChildrenSortedBy(selector: (View) -> T2) = this.keepChildrenSortedBy(kotlin.Comparator { a, b -> selector(a).compareTo(selector(b)) })
+
+//fun <T : Container> T.keepChildrenSortedBy(comparator: Comparator<View>): T = this.apply { SortedChildrenByComponent(this, comparator).attach() }
+//fun <T : Container, T2 : Comparable<T2>> T.keepChildrenSortedBy(selector: (View) -> T2): T = this.keepChildrenSortedBy(kotlin.Comparator { a, b -> selector(a).compareTo(selector(b)) })
+//fun <T : Container> T.keepChildrenSortedByY(): T = this.keepChildrenSortedBy(View::y)
+//fun <T : Container, T2 : Comparable<T2>> T.keepChildrenSortedBy(selector: (View) -> T2): T = this.keepChildrenSortedBy(kotlin.Comparator { a: View, b: View -> selector(a).compareTo(selector(b)) })
+
+fun <T : Container> T.keepChildrenSortedBy(comparator: Comparator<View>): T =
 	this.apply { SortedChildrenByComponent(this, comparator).attach() }
 
-fun <T : Container, T2 : Comparable<T2>> T.keepChildrenSortedBy(selector: (View) -> T2) =
-	this.keepChildrenSortedBy(kotlin.Comparator { a, b -> selector(a).compareTo(selector(b)) })
+fun <T : Container, T2 : Comparable<T2>> T.keepChildrenSortedBy(selector: (View) -> T2): T =
+	this.keepChildrenSortedBy(Comparator { a: View, b: View -> selector(a).compareTo(selector(b)) })
 
-fun <T : Container> T.keepChildrenSortedByY() = this.keepChildrenSortedBy(View::y)
+fun <T : Container> T.keepChildrenSortedByY(): T = this.keepChildrenSortedBy(View::y)
