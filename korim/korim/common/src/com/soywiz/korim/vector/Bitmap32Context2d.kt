3ad6950a60c576cfb650ec1fc1aed5b683b72bea
soywiz <soywiz@gmail.com>
2018-06-25 07:32:51 +0200

    Do not throw exception in render + some useful links for implementing it

--- korim/korim/common/src/com/soywiz/korim/vector/Bitmap32Context2d.kt
+++ korim/korim/common/src/com/soywiz/korim/vector/Bitmap32Context2d.kt
@@ -1,13 +1,18 @@
 package com.soywiz.korim.vector
 
 import com.soywiz.korim.bitmap.*
+import com.soywiz.korim.color.*
 
+// References:
+// - https://github.com/memononen/nanosvg/blob/master/src/nanosvgrast.h
+// - https://www.geeksforgeeks.org/scan-line-polygon-filling-using-opengl-c/
+// - https://hackernoon.com/computer-graphics-scan-line-polygon-fill-algorithm-3cb47283df6
 class Bitmap32Context2d(val bmp: Bitmap32) : Context2d.Renderer() {
 	override val width: Int get() = bmp.width
 	override val height: Int get() = bmp.height
 
 	override fun render(state: Context2d.State, fill: Boolean) {
-		// @TODO: https://github.com/memononen/nanosvg/blob/master/src/nanosvgrast.h
-		TODO("Not implemented context2d on Bitmap32, please use NativeImage instead")
+		println("WARNING: Not implemented context2d on Bitmap32, please use NativeImage instead. Filled the image with PINK.")
+		bmp.fill(Colors.PINK)
 	}
 }
