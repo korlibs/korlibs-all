a8ba998a79ac0c96e9c587de9ce8b495e0d8b21b
soywiz <soywiz@gmail.com>
2018-06-26 05:59:05 +0200

    Added missing imports

--- korui/korui/native/src/com/soywiz/korui/KoruiEventLoopNative.kt
+++ korui/korui/native/src/com/soywiz/korui/KoruiEventLoopNative.kt
@@ -2,6 +2,7 @@ package com.soywiz.korui
 
 import com.soywiz.korio.async.*
 import com.soywiz.korui.light.*
+import com.soywiz.korio.*
 
 actual object KoruiEventLoop {
 	actual val instance: EventLoop by lazy { SdlEventLoop() }
