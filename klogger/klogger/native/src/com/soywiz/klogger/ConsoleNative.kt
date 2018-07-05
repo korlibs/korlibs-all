48c16ffd72ab4f14e506ab82e8c53eb5d944cc7a
soywiz <soywiz@gmail.com>
2018-07-05 20:49:33 +0200

    Remove fflush(__stdoutp)

--- klogger/klogger/native/src/com/soywiz/klogger/ConsoleNative.kt
+++ klogger/klogger/native/src/com/soywiz/klogger/ConsoleNative.kt
@@ -1,15 +1,11 @@
 package com.soywiz.klogger
 
-import platform.posix.*
-
 actual inline fun Console.error(vararg msg: Any?) {
 	println(msg.joinToString(", "))
-	fflush(__stdoutp)
 }
 
 actual inline fun Console.log(vararg msg: Any?) {
 	println(msg.joinToString(", "))
-	fflush(__stdoutp)
 }
 
 actual typealias ConsoleThreadLocal = konan.ThreadLocal
