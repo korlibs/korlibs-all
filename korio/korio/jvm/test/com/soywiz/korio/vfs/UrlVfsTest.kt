390ca4713eb1d5347fb3e1a178e8ead63e5621b2
soywiz <soywiz@gmail.com>
2018-07-08 19:31:55 +0200

    Added check testing UrlVfs allow parents without jail

--- korio/korio/jvm/test/com/soywiz/korio/vfs/UrlVfsTest.kt
+++ korio/korio/jvm/test/com/soywiz/korio/vfs/UrlVfsTest.kt
@@ -1,6 +1,7 @@
 package com.soywiz.korio.vfs
 
 import com.soywiz.korio.async.*
+import com.soywiz.korio.file.std.*
 import com.soywiz.korio.net.http.*
 import kotlin.test.*
 
@@ -9,18 +10,18 @@ class UrlVfsTest {
 	fun name() = suspendTest {
 		assertEquals(
 			"http://test.com/demo/hello/world",
-			com.soywiz.korio.file.std.UrlVfs("http://test.com/")["demo"].jail()["hello/world"].absolutePath
+			UrlVfs("http://test.com/")["demo"].jail()["hello/world"].absolutePath
 		)
 		assertEquals(
 			"http://test.com/demo/hello/world",
-			com.soywiz.korio.file.std.UrlVfs("http://test.com/")["/demo"].jail()["/hello/world"].absolutePath
+			UrlVfs("http://test.com/")["/demo"].jail()["/hello/world"].absolutePath
 		)
 	}
 
 	@Test
 	fun testRightRequests() = suspendTest {
 		val httpClient = LogHttpClient()
-		val url = com.soywiz.korio.file.std.UrlVfs("http://google.es/", httpClient)
+		val url = UrlVfs("http://google.es/", httpClient)
 		println(url.readString())
 		assertEquals(
 			listOf("GET, http://google.es/, Headers(), null"),
@@ -31,11 +32,20 @@ class UrlVfsTest {
 	@Test
 	fun requestRightUrl() = suspendTest {
 		val httpClient = LogHttpClient()
-		val url = com.soywiz.korio.file.std.UrlVfs("http://google.es/demo/file.png", httpClient)
+		val url = UrlVfs("http://google.es/demo/file.png", httpClient)
 		println(url.readString())
 		assertEquals(
 			"[GET, http://google.es/demo/file.png, Headers(), null]",
 			httpClient.getAndClearLog().toString()
 		)
 	}
+
+	@Test
+	fun testUrlParent() = suspendTest {
+		assertEquals(
+			"http://test.com/hello/",
+			UrlVfs("http://test.com/hello/world")[".."].jail().absolutePath
+		)
+	}
+
 }
