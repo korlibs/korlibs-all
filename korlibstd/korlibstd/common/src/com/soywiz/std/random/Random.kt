7b4ed98343dca5522516ffb69e564c895f04ec6c
soywiz <soywiz@gmail.com>
2018-09-26 23:11:24 +0200

    WIP new mpp (non working native dependencies)

--- korlibstd/korlibstd/common/src/com/soywiz/std/random/Random.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.std.random
-
-expect object Random {
-	fun nextInt(): Int
-}
-
-fun Random.nextDouble(): Double = (nextInt() and 0x7FFFFFFF).toDouble() / 0x7FFFFFFF.toDouble()
-fun Random.nextLong(): Long = (nextInt().toLong() shl 32) or nextInt().toLong()
--- korlibstd/korlibstd/jvm/src/com/soywiz/std/random/Random.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.std.random
-
-actual object Random {
-	private val jrand = java.util.Random()
-	actual fun nextInt(): Int {
-		return jrand.nextInt()
-	}
-}
