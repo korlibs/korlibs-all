44d6ca015b449fbe8c1e36170dda5545ca253b49
soywiz <soywiz@gmail.com>
2018-10-14 18:13:47 +0200

    Moved klogger to https://github.com/korlibs/klogger

--- klogger/src/jsMain/kotlin/com/soywiz/klogger/ConsoleJs.kt
+++
@@ -1,4 +0,0 @@
-package com.soywiz.klogger
-
-actual inline fun Console.error(vararg msg: Any?) = console.error(*msg)
-actual inline fun Console.log(vararg msg: Any?) = console.log(*msg)
--- klogger/src/nativeCommonMain/kotlin/com/soywiz/klogger/ConsoleNative.kt
+++
@@ -1,9 +0,0 @@
-package com.soywiz.klogger
-
-actual inline fun Console.error(vararg msg: Any?) {
-	println(msg.joinToString(", "))
-}
-
-actual inline fun Console.log(vararg msg: Any?) {
-	println(msg.joinToString(", "))
-}
