de6e0d8c68fac699bd3621bf66479f7689799844
soywiz <soywiz@gmail.com>
2018-10-15 02:03:25 +0200

    Removed old stuff

--- korlibstd/src/jsMain/kotlin/com/soywiz/std/random/Random.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.std.random
-
-actual object Random {
-	actual fun nextInt(): Int = nextInt16() or (nextInt16() shl 16)
-
-	@Suppress("DEPRECATION")
-	private fun nextInt16(): Int = kotlin.math.round((kotlin.random.Random.nextDouble() * 0xFFFF)).toInt()
-}
