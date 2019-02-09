66044f14022518ea4a56c026488280e654ca9f9d
soywiz <soywiz@gmail.com>
2019-02-09 11:46:37 +0100

    Moved old stuff

--- samples/sample1/jvm/src/main.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) = example2.main(args)
-
-//fun main(args: Array<String>): Unit = Korio {
-//    val vfs = MemoryVfsMix("hello" to "WORLD")
-//    println("HELLO ${vfs["hello"].readString()} FROM KORIO")
-//}
-
